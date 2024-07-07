79ef55ba4eb9c43e435769304f132bd85442f330
satvik5678 <satvikbhanwal5678@gmail.com>
2024-07-08 00:09:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnected", function () {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+server.listen(3030);
